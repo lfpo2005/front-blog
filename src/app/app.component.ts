39e964b7fbb67a9a667e7e2bf86e3c598d01eb7e
lfpo2005 <lfpo2005@yahoo.com.br>
2023-05-03 08:55:21 -0300

    correção so search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PostModel} from "./shared/models/post.model";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'front-blog';
+export class AppComponent implements OnInit {
+  listPosts: PostModel[] | null = null;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onSearchResultsChanged(searchResults: PostModel[]) {
+    this.listPosts = searchResults;
+  }
+
 }
