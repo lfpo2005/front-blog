b6accd5b9e6032fdfefaef2c4fa91860ca7c5f27
lfpo2005 <lfpo2005@yahoo.com.br>
2023-05-15 20:30:02 -0300

    prod

--- src/app/post-editor/post-editor.component.ts
+++ src/app/post-editor/post-editor.component.ts
@@ -36,8 +36,8 @@ export class PostEditorComponent implements OnInit {
     this.editorConfig = {
       height: 200,
       placeholder: 'Digite o conteúdo da postagem',
-      plugins: 'lists link code fullscreen preview autosave image imagetools emoticons autoresize',
-      toolbar: 'undo redo | bold italic underline | fontselect fontsizeselect | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | code | fullscreen | preview | autosave | emoticons | table',
+      plugins: 'lists link code fullscreen preview image emoticons autoresize',
+      toolbar: 'undo redo | bold italic underline | fontselect fontsizeselect | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | code | fullscreen | preview | autosave | emoticons',
       file_picker_callback: (cb: any, value: any, meta: any) => {
         const input = document.createElement('input');
         input.setAttribute('type', 'file');
