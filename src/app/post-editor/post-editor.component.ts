d59494097e70d974b5980c69c8dc6a02f414f2d7
lfpo2005 <lfpo2005@yahoo.com.br>
2023-05-18 12:35:31 -0300

    summernote

--- src/app/post-editor/post-editor.component.ts
+++ src/app/post-editor/post-editor.component.ts
@@ -4,8 +4,8 @@ import { PostModel } from '../shared/models/post.model';
 import { BlogService } from '../shared/services/blog.service';
 import { Router } from '@angular/router';
 import { DatePipe } from "@angular/common";
-import { HttpEventType, HttpResponse } from '@angular/common/http';
 import 'summernote/dist/summernote-bs5.js';
+import { HttpEventType, HttpResponse } from "@angular/common/http";
 
 declare let $: any;
 declare let document: any;
@@ -17,6 +17,7 @@ declare let document: any;
 })
 export class PostEditorComponent implements OnInit {
   selectedFile: File | null = null;
+
 
   @Output() postCreated = new EventEmitter<PostModel>();
   postForm: FormGroup;
@@ -34,20 +35,49 @@ export class PostEditorComponent implements OnInit {
       tags: [''],
     });
   }
-
   ngOnInit(): void {
     this.postForm.valueChanges.subscribe(value => {
     });
   }
-
   onSubmit() {
-    // ... your existing onSubmit code ...
-  }
+    if (this.postForm.valid) {
+      const post: PostModel = this.postForm.value;
+      const tagsInput = this.postForm.get('tags')?.value;
+      if (typeof tagsInput === 'string') {
+        post.tags = tagsInput.split(',').map((tag: string) => tag.trim());
+      } else {
+        post.tags = [];
+      }
+      const formData = new FormData();
+      formData.append('title', post.title ?? '');
+      formData.append('post', post.post ?? '');
+      formData.append('description', post.description ?? '');
+      formData.append('tags', post.tags.join(','));
 
+      if (this.selectedFile) {
+        formData.append('img', this.selectedFile, this.selectedFile.name);
+      }
+      this.blogService.createPosts(formData).subscribe(
+        event => {
+          if (event.type === HttpEventType.UploadProgress) {
+          } else if (event instanceof HttpResponse) {
+            alert("Post criado com sucesso!");
+            const postId = event.body.postId;
+            this.postForm.reset();
+            this.router.navigate(['/postDetails', postId]);
+          }
+        },
+        (err) => {
+          alert("Erro ao criar post!");
+        }
+      );
+    } else {
+      alert("Erro ao criar post!");
+    }
+  }
   onFileSelected(event: any) {
     this.selectedFile = event.target.files[0];
   }
-
   ngAfterViewInit() {
     $(document).ready(function() {
       $('.summernote').summernote();
