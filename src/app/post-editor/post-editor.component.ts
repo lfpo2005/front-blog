457b38a3172f2f195809f12152c029d6afb8c2e8
lfpo2005 <lfpo2005@yahoo.com.br>
2023-05-19 20:57:51 -0300

    add img

--- src/app/post-editor/post-editor.component.ts
+++ src/app/post-editor/post-editor.component.ts
@@ -20,20 +20,10 @@ export class PostEditorComponent implements OnInit {
   selectedFile: File | null = null;
   content: string = '';
   config = {
-    placeholder: '',
+    placeholder: 'Conteúdo do post',
     tabsize: 2,
     height: 200,
-    // uploadImagePath: '/upload-body-img',
-   /* toolbar: [
-      ['misc', ['codeview', 'undo', 'redo']],
-      ['style', ['bold', 'italic', 'underline', 'clear']],
-      ['font', ['bold', 'italic', 'underline', 'strikethrough', 'superscript', 'subscript', 'clear']],
-      ['fontsize', ['fontname', 'fontsize', 'color']],
-      ['para', ['style', 'ul', 'ol', 'paragraph', 'height']],
-      ['insert', ['table', 'picture', 'link', 'video', 'hr']]
-    ],
-    fontNames: ['Helvetica', 'Arial', 'Arial Black', 'Comic Sans MS', 'Courier New', 'Roboto', 'Times'],*/
-    popover: {
+     popover: {
       toolbar: [
         ['misc', ['codeview', 'undo', 'redo']],
         ['style', ['bold', 'italic', 'underline', 'clear']],
